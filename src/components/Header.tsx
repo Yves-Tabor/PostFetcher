d92ebad5f309277f9e6fb073751376510ad23b8f
Yves-Tabor <walvesoutis.gmail.com>
2026-04-09 17:19:19 +0200

    Header created

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Post Fetcher</h1>
+            <h4>Enter A URL to fetch and display posts</h4>
+        </header>
+    )
+}
+
+export default Header
